7f52fc071aca6a08d3cf68a62d961c779fe1c85a
DanielSharp01 <DanielSharp01@gmail.com>
2019-03-07 16:46:30 +0100

    Restructured

--- gulpfile.js
+++ gulpfile.js
@@ -7,19 +7,30 @@ const uglify = require("gulp-uglify");
 const autoprefixer = require("gulp-autoprefixer");
 
 gulp.task("sass-compile", () => {
-    return gulp.src("sass/[^_]*.scss")
+    return gulp.src("src/sass/[^_]*.scss")
         .pipe(sass().on("error", sass.logError))
         .pipe(clip())
         .pipe(autoprefixer())
-        .pipe(gulp.dest("static/css"));
+        .pipe(gulp.dest("public/css"));
 });
 
-gulp.task("bootstrap", () =>
+gulp.task("js-copy", () => {
+    return gulp.src("src/client/*.js")
+        .pipe(gulp.dest("public/js"));
+});
+
+gulp.task("bootstrap-js", () =>
 {
     return gulp.src("node_modules/bootstrap/js/dist/*.js")
         .pipe(concat("bootstrap-min.js"))
         .pipe(uglify())
-        .pipe(gulp.dest("static/js"));
+        .pipe(gulp.dest("public/js"));
+});
+
+
+gulp.task("build", () =>
+{
+    gulp.series("bootstrap-js", "sass-compile", "js-copy");
 });
 
 gulp.task("watch", () =>
